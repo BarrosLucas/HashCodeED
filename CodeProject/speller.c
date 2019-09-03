ccd9ea474a4bc67b54fe0d98d05519c27d538a05
danpires97 <daniel.pires@lavid.ufpb.br>
2019-09-03 15:54:15 -0300

    Quase la

--- CodeProject/speller.c
+++ CodeProject/speller.c
@@ -1,13 +1,17 @@
-#include <windows.h>
-#include <winbase.h>
 #include <stdio.h>
-#include "dictionary.h"
+#include <stdlib.h>
+#include "dictionary.c"
+#include <time.h>
+#include <math.h>
 
-int have=0,dont_have=0;
+int have=0,dont_have=0, qtdWord=0;
 int main(){
-    __int64 freq,start,stop;
+    //__int64 freq,start,stop;
+    time_t inicio,fim, myTime;
+
+
     double time = 0;
-    QueryPerformanceFrequency((LARGE_INTEGER *)&freq);
+    //QueryPerformanceFrequency((LARGE_INTEGER *)&freq);
     if(load()){
         printf("Carregou o dicionário\n");
     }else{
@@ -15,15 +19,46 @@ int main(){
     }
 
     char word[LENGTH+1];
-    FILE* the_constitution = fopen("../Anexos/constituicao.txt", "r");
+    char words[60714][38];
+    FILE* the_constitution = fopen("constituicao.txt", "r");
+    FILE* relatorio = fopen("relatorio.txt", "w");
 
     while (fscanf(the_constitution, "%s", word) > 0){
-        QueryPerformanceCounter((LARGE_INTEGER *)&start);
-        (check(word)? have++:dont_have++);
-        QueryPerformanceCounter((LARGE_INTEGER *)&stop);
-        time += ((double)stop-(double)start)/(double)freq;
+        //QueryPerformanceCounter((LARGE_INTEGER *)&start);
+        inicio = clock();
+        bool exist = check(word);
+        fim = clock();
+        if (exist){
+            have++;
+        }else{
+            strcpy(words[dont_have],word);
+            dont_have++;
+        }
+        
+        //QueryPerformanceCounter((LARGE_INTEGER *)&stop);
+        //time += ((double)stop-(double)start)/CLOCKS_PER_SEC;
+        myTime += (inicio-fim)/CLOCKS_PER_SEC;
     }
 
+    qtdWord = have + dont_have;
+
+    fprintf(relatorio, "Numero total de palavras do texto: %d\n", qtdWord);
+    fprintf(relatorio, "Número de palavras que falharam no spell check: %d\n", dont_have);
+    fprintf(relatorio, "Tempo total da verificação: %ld\n", myTime);
+    fprintf(relatorio, "Lista de palavras que falharam no spell check:\n");
+    fprintf(relatorio, "Num. Ocorrencia - Palavra\n");
+    fprintf(relatorio, "-----------------------------------------\n");
+
+    printf("Palavras que nao tem: \n");
+    int i;
+    for(i=0;i<dont_have;i++){
+        fprintf(relatorio, "%d - %s\n",i+1,words[i]);
+    }
+
+
+    /*relatorio<<"Numero total de palavras: "<<qtdWord<<"\n";
+    relatorio<<"Numero de palavras que falharam no spell check:"<<dont_have;
+    relatorio<<"Tempo de execucao: "<<myTime<<"\n"; */
 
     if(unload()){
         printf("Descarregou o dicionário\n");
@@ -33,6 +68,7 @@ int main(){
 
     printf("Tem: %d\nNao tem: %d\n",have,dont_have);
     //printf("Total ms: %g ms.\n",total);
-    printf("Clock: %lf\n",time);
+    printf("Clock: %ld\n",myTime);
+    fclose(the_constitution);
     return 0;
 }
